9c203eb63ccf2703d22b8872af7028a5bcb63f0c
Daniel Blackburn <craklyn@gmail.com>
2018-02-10 12:43:34 -0800

    small additions

--- app-frontend/routes/index.js
+++ app-frontend/routes/index.js
@@ -1,9 +1,15 @@
+var path = require('path');
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/index', function(req, res) {
+    fs.readFile('views/index.html', function (err, html) {
+         res.writeHeader(200, {"Content-Type": 'text/html'});
+         res.write(html);
+         res.end();
+    });
 });
+
 
 module.exports = router;
