8cbec5d29f235192da7fbc0928575ef05556950f
Daniel Blackburn <craklyn@gmail.com>
2018-02-10 18:02:01 -0800

    Improved various stylizations

--- app-frontend/routes/index.js
+++ app-frontend/routes/index.js
@@ -11,5 +11,13 @@ router.get('/index', function(req, res) {
     });
 });
 
+router.get('/list', function(req, res) {
+    fs.readFile('views/listReport.html', function (err, html) {
+         res.writeHeader(200, {"Content-Type": 'text/html'});
+         res.write(html);
+         res.end();
+    });
+});
+
 
 module.exports = router;
