5d25755d75f3bfb0e53eadde5a4523f92a4ca4bc
Daniel Blackburn <craklyn@gmail.com>
2018-02-11 11:02:09 -0800

    setting up infrastructure for presentation

--- app-frontend/routes/index.js
+++ app-frontend/routes/index.js
@@ -27,6 +27,72 @@ router.get('/invasive', function(req, res) {
     });
 });
 
+router.get('/page1', function(req, res) {
+    fs.readFile('views/page1.html', function (err, html) {
+         res.writeHeader(200, {"Content-Type": 'text/html'});
+         res.write(html);
+         res.end();
+    });
+});
+
+router.get('/page2', function(req, res) {
+    fs.readFile('views/page2.html', function (err, html) {
+         res.writeHeader(200, {"Content-Type": 'text/html'});
+         res.write(html);
+         res.end();
+    });
+});
+
+router.get('/page3', function(req, res) {
+    fs.readFile('views/page3.html', function (err, html) {
+         res.writeHeader(200, {"Content-Type": 'text/html'});
+         res.write(html);
+         res.end();
+    });
+});
+
+router.get('/page4', function(req, res) {
+    fs.readFile('views/page4.html', function (err, html) {
+         res.writeHeader(200, {"Content-Type": 'text/html'});
+         res.write(html);
+         res.end();
+    });
+});
+
+router.get('/page5', function(req, res) {
+    fs.readFile('views/page5.html', function (err, html) {
+         res.writeHeader(200, {"Content-Type": 'text/html'});
+         res.write(html);
+         res.end();
+    });
+});
+
+router.get('/page6', function(req, res) {
+    fs.readFile('views/page6.html', function (err, html) {
+         res.writeHeader(200, {"Content-Type": 'text/html'});
+         res.write(html);
+         res.end();
+    });
+});
+
+router.get('/page7', function(req, res) {
+    fs.readFile('views/page7.html', function (err, html) {
+         res.writeHeader(200, {"Content-Type": 'text/html'});
+         res.write(html);
+         res.end();
+    });
+});
+
+router.get('/page8', function(req, res) {
+    fs.readFile('views/page8.html', function (err, html) {
+         res.writeHeader(200, {"Content-Type": 'text/html'});
+         res.write(html);
+         res.end();
+    });
+});
+
+
+
 router.use('/static', express.static(path.join(__dirname, 'public')))
 
 
